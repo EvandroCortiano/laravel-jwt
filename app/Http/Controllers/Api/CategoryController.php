7e3612ba23b8bc63d5b814aa00a72c93a48fc303
EvandroCortiano <evandrocortiano@gmail.com>
2018-11-05 23:57:29 -0200

    Api Resource

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -6,31 +6,32 @@ use App\Models\Category;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\CategoryRequest;
+use App\Http\Resources\CategoryResource;
 
 class CategoryController extends Controller
 {
     public function index()
     {
         $categories = Category::paginate();
-        return $categories;
+        return CategoryResource::collection($categories);
     }
 
     public function store(CategoryRequest $request)
     {
         $category = Category::create($request->all());
-        return $category;
+        return new CategoryResource($category);
     }
 
     public function show(Category $category)
     {
-        return $category;
+        return new CategoryResource($category);
     }
 
     public function update(CategoryRequest $request, Category $category)
     {
         $category->fill($request->all());
         $category->save();
-        return $category;
+        return new CategoryResource($category);
     }
 
     public function destroy(Category $category)
